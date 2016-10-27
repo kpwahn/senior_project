1648c0b8e06d250c19aafb633ee904b80500f5ab
kpwahn <kpwahnschaffe@gmail.com>
2016-10-27 12:45:44 -0600

    Adding new ssl certificate

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 var fs = require('fs');
 
 var hskey = fs.readFileSync('hacksparrow-key.pem');
-var hscert = fs.readFileSync('hacksparrow-cert.pem')
+var hscert = fs.readFileSync('hacksparrow-cert.pem');
 
 var options = {
     key: fs.readFileSync('54_244_96_10.key'),
