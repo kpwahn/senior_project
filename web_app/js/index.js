d9f4737981ef325d656d9ca1074ccee63291c85e
kpwahn <kpwahnschaffe@gmail.com>
2016-10-27 12:15:46 -0600

    Changing base URL to server

--- web_app/js/index.js
+++ web_app/js/index.js
@@ -46,8 +46,8 @@ bankApp.factory('loginService', function($http){
 	
 	var factory = {}
 	
-	//factory.baseURL = "http://54.244.96.10";
-	factory.baseURL = "http://127.0.0.1:8080";
+	factory.baseURL = "http://54.244.96.10";
+	//factory.baseURL = "http://127.0.0.1:8080";
 	
 	factory.previousPage = "#/";
 	
