ba771341b8ca4ccd7ade536a208e6dec1b3b4f3b
kpwahn <kpwahnschaffe@gmail.com>
2016-11-08 11:32:22 -0700

    fixing token expiration handling on client side

--- web_app/js/controllers/homeController.js
+++ web_app/js/controllers/homeController.js
@@ -21,6 +21,7 @@ angular.module('bankApp').controller('homeController', ['$scope', '$http', 'logi
 					alert("Your session has timed out. Please log in and try again");
 					loginService.previousPage = "#/transactions"
 					window.location.href = "#/login";	
+					break;
 				}
 				$scope.accounts = data;
 
