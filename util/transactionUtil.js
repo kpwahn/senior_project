b1fa2b97807e2a5c8d14a7b1b6b1f3947b23ab27
kpwahn <kpwahnschaffe@gmail.com>
2016-11-09 16:15:42 -0700

    Server side formatting of amounts

--- util/transactionUtil.js
+++ util/transactionUtil.js
@@ -1,13 +1,13 @@
 var Transaction = require('./../database/models/transaction');
 var Account = require('./../database/models/account');
-var util = require('util');
+var Util = require('util');
 
 exports.makeTransaction = function(info, callback){
 	transaction = new Transaction();
 	
 	transaction.date = new Date();
 	transaction.type = info.type;
-	transaction.amount = util.formatAmount(info.amount);
+	transaction.amount = Util.formatAmount(info.amount);
 	
 	switch(transaction.type){
 		case "purchase":
