7fb31ba8ffb9c8c6a5049ebaa992ab28835121d4
kpwahn <kpwahnschaffe@gmail.com>
2016-11-14 17:55:46 -0700

    preparing help endpoint

--- util/helpUtil.js
+++ util/helpUtil.js
@@ -4,8 +4,41 @@ exports.help = function(callback) {
 		base_URL : "https://kpwahnschaffe.com/",
 		last_Updated : "November 14, 2016",
 		endpoints : {
-			"/" : "Returns the default static website",
-			"/help" : "Returns JSON explaining the API"
+			"/" : {
+				"Method" : "GET",
+				"requires_authentication" : false,
+				"description" : "Returns the default static website"
+			},
+			"http://kpwahnschaffe.com/*" : {
+				"Method" : "GET",
+				"requires_authentication" : false,
+				"description" : "(http) Returns the default status website, no matter the route. Cannot access API with http"
+			},
+			"/help" : {
+				"Method" : "GET",
+				"requires_authentication" : false,
+				"description" : "Returns JSON explaining the API"
+			},
+			"/createNewMember" : {
+				"Method" : "POST",
+				"requires_authentication" : false
+			},
+			"/makeTransaction" : {
+				"Method" : "POST",
+				"requires_authentication" : true
+			},
+			"/getAccounts" : {
+				"Method" : "POST",
+				"requires_authentication" : true
+			},
+			"/createAccount" : {
+				"Method" : "POST",
+				"requires_authentication" : true
+			},
+			"/authenticate" : {
+				"Method" : "POST",
+				"requires_authentication" : true
+			}
 		}
 	}
 	
