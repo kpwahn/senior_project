3b7335e6f7f8bd54d158715f01131465c3dbcda0
kpwahn <kpwahnschaffe@gmail.com>
2016-12-02 16:37:41 -0700

    Testing

--- util/util.js
+++ util/util.js
@@ -11,8 +11,10 @@ exports.formatAmount = function(amount){
 }
 
 exports.isValidAmount = function(amount){
+	console.log(amount);
 	// Can't seem to get the regex to not allow a '$', so this.
 	if(amount[0] = '$')
 		return false
+		console.log(!!amount.match(/(?=.)(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/));
 	return !!amount.match(/(?=.)(([1-9][0-9]{0,2}(,[0-9]{3})*)|[0-9]+)?(\.[0-9]{1,2})?$/)
 }
