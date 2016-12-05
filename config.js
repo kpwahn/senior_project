46e1c09b77e5f8b9bcfa8fcc448315bde22023cb
kpwahn <kpwahnschaffe@gmail.com>
2016-12-05 15:35:21 -0700

    implementing express-rate-limit

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
 		"delayMs": 0 // diable delaying (full speed until max limit is reached)
 	},
 	"database": {
-		"path": 'mongodb://localhost:27017/bank',
-		"secret": 'ilovescotchyscotch'
+		"path": "mongodb://localhost:27017/bank",
+		"secret": "ilovescotchyscotch"
 	}
 };
