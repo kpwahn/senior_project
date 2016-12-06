4bfcec4a7ff8b6a80100791f95878c0f04df6211
kpwahn <kpwahnschaffe@gmail.com>
2016-12-06 14:49:32 -0700

    adding remove accounts endpoint

--- util/accountUtil.js
+++ util/accountUtil.js
@@ -45,13 +45,10 @@ exports.createAccount = function(info, callback){
 }
 
 exports.removeAccount = function(info, callback) {
-	console.log(info);
-	
-	Account.remove({"member_id": info.member_id, "account_number": info.account_number}, { justOne: true }, function(err, account) {
+	Account.remove({"member_id": info.member_id, "account_number": info.account_number}, function(err, account) {
 		if (err) {
 			callback({status: 400, data: err});	
 		} else {
-			console.log("something something " + account);
 			callback(account);
 		}
 	});
