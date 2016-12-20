eb7daefa764d9756d9b6f98231e6ab2b2ef20f62
kpwahn <kpwahnschaffe@gmail.com>
2016-12-20 11:02:52 -0700

    adding stuff for mee

--- server.js
+++ server.js
@@ -243,8 +243,7 @@ app.get('/help', function(req, res) {
 app.get('/words', function(req, res) {
 	var fs = require("fs");
 	var text = fs.readFileSync("./words.txt");
-	console.log(text);
-	var textByLine = text.split("\n")
+	console.log("HERE: " + text);
 });
 
 var port = 443;
